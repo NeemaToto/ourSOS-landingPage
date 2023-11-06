b1cb0c685da84d522e69fc1020c8322147ef1a22
Neema Totonchi <neematotonchi@Neemas-MacBook-Pro.local>
2023-11-05 23:05:09 -0800

    initial commit

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Get started with the template by clicking `Use this template` button on the top of the page.
 
 [Documentation](https://mantine.dev/guides/next/)
+
+# This is a landing page for OurSOS - Anxious Travelers
+
+## Uses Mantine UI
+
+## Installations
+```
+npm install --save @lottiefiles/lottie-player
+npm install @mantine/form
+```
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import { Button, Group } from "@mantine/core";
+import React, { useRef } from "react";
+import styles from 'styles/Home.module.css'
+
 
 export default function IndexPage() {
+  const ref = useRef(null);
+  React.useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
+
   return (
-    <Group mt={50} justify="center">
-      <Button size="xl">Welcome to Mantine!</Button>
-    </Group>
+    <div>
+      <div>
+        <lottie-player
+          id="firstLottie"
+          ref={ref}
+          autoplay
+          mode="normal"
+          src="animation/introPageAnimation.json"
+          style={{ width: "100vw", height: "100vh" }}
+        />
+      </div>
+      <div>
+
+      </div>
+    </div>
   );
 }
