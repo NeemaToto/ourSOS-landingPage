4a4d42545d59d82b46cf52c6fd298cdd1bd61e6b
Neema Totonchi <neematotonchi@Neemas-MacBook-Pro.local>
2023-11-06 18:42:54 -0800

    apos

--- components/Newsletter/index.tsx
+++ components/Newsletter/index.tsx
@@ -9,11 +9,12 @@ export function NewsLetter() {
                 <div className={classes.wrapper}>
                     <div className={classes.contacts} style={{ backgroundColor: '#003566' }}>
                         <Title order={2} fw={700} className={classes.title} c="#FFF">
-                            Subscribe to the OurSOS <span style={{color: '#FFD400'}}>Newsletter</span>
+                            Subscribe to the OurSOS <span style={{ color: '#FFD400' }}>Newsletter</span>
                         </Title>
                         <Text size='sm' c="#fff">
-                        Subscribe to our newsletter to be in the know about everything related to OurSOS. You'll have the inside track on new blog posts and even be the first to know when we launch OurSOS.</Text>
-
+                            Subscribe to our newsletter to be in the know about everything related to OurSOS. You&apos;ll have the inside track on new blog posts and even be the first to know when we launch OurSOS.
+                        </Text>
+                    
                     </div>
 
                     <form className={classes.form} onSubmit={(event) => event.preventDefault()}>
@@ -22,9 +23,9 @@ export function NewsLetter() {
                         </Title>
 
                         <div className={classes.fields}>
-                            <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem'}}>
-                            <TextInput label="Your name" placeholder="Your name" />
-                            <TextInput label="Your email" placeholder="Name@email.com" required />
+                            <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+                                <TextInput label="Your name" placeholder="Your name" />
+                                <TextInput label="Your email" placeholder="Name@email.com" required />
                             </div>
                             <Group justify="flex-start" mt="md">
                                 <Button type="submit" color='#FFD400' className={classes.control}>
